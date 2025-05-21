354eca445fc50d10b028f22598c8bf2df705f19b
Thirishaa <thirishaaswamynathan@gmail>
2025-05-22 00:52:56 +0800

    feat(mapper): add RowMappers for Book, Member, and Borrow entities

--- src/main/java/com/library/library_management/mapper/BorrowRowMapper.java
+++ src/main/java/com/library/library_management/mapper/BorrowRowMapper.java
@@ -1,5 +1,20 @@
 package com.library.library_management.mapper;
 
-public class BorrowRowMapper {
+import com.library.library_management.model.Borrow;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BorrowRowMapper implements RowMapper<Borrow> {
+    @Override
+    public Borrow mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Borrow borrow = new Borrow();
+        borrow.setId(rs.getLong("id"));
+        borrow.setMemberId(rs.getLong("member_id"));
+        borrow.setBookId(rs.getLong("book_id"));
+        borrow.setBorrowDate(rs.getDate("borrowed_date").toLocalDate());
+        borrow.setDueDate(rs.getDate("due_date").toLocalDate());  
+        return borrow;
+    }
 }
--- src/main/java/com/library/library_management/mapper/MemberRowMapper.java
+++ src/main/java/com/library/library_management/mapper/MemberRowMapper.java
@@ -1,5 +1,20 @@
 package com.library.library_management.mapper;
 
-public class MemberRowMapper {
+import com.library.library_management.model.Member;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class MemberRowMapper implements RowMapper<Member> {
+    @Override
+    public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Member member = new Member();
+        member.setId(rs.getInt("id"));
+        member.setName(rs.getString("name"));
+        member.setPhone(rs.getString("phone"));
+        member.setRegisteredDate(rs.getDate("registered_date").toLocalDate());
+        return member;
+    }
 }
